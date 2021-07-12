1f4bd93a6b699df33b15e837988121796a467f21
酒井友太郎 <>
2021-07-12 12:35:57 +0900

    profile model を作成 user model と profile model を紐付け（1対1）

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy #このユーザーが削除されたら紐づく記事も消す
+  has_one :profile, dependent: :destroy
 
   def has_written?(article)
     articles.exists?(id: article.id)
