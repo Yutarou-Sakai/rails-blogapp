d29ed93fd9a01af5167324053931eecc35189fc2
酒井友太郎 <>
2021-07-20 18:51:45 +0900

    articleのダミーデータをfactory_botで書き換え

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,25 +3,23 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
+
   context 'タイトルと内容が入力されている場合' do
-    let!(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 100)
-      })
-    end
+    let!(:article) { build(:article, user: user) }
+
     it '記事を保存できる' do
       expect(article).to be_valid
     end
   end
 
+
   context 'タイトルが一文字の場合' do
-    let!(:article) do
-      user.articles.create({
-        title: Faker::Lorem.characters(number: 1),
-        content: Faker::Lorem.characters(number: 100)
-      })
+    let!(:article) { build(:article, title: Faker::Lorem.characters(number: 1), user: user) }
+
+    before do
+      article.save
     end
+
     it '記事を保存できない' do
       expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
